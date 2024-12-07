5af158c9f7cbe0dfcb6943ceb71c73ae85cfdf9a
3wiida <mahmoudewida5@gmail.com>
2024-12-07 16:46:54 +0200

    LeetCode => Arrays => Two Sum

---
+++ LeetCode/Arrays/Two Sum.java
@@ -0,0 +1,27 @@
+/*
+    Problem Link: https://leetcode.com/problems/two-sum/description/?envType=problem-list-v2&envId=array&difficulty=EASY
+    Difficulty: Easy
+    Solved with: Java
+ */
+
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        int firstNumIndex = 0, secondNumIndex = 0, sum;
+        boolean stopFlag = false;
+
+        for (int i = 0; i < nums.length; i++) {
+            firstNumIndex = i;
+            for (int j = i + 1; j < nums.length; j++) {
+                secondNumIndex = j;
+                sum = nums[i] + nums[j];
+                if(sum == target){
+                    stopFlag = true;
+                    break;
+                }
+            }
+            if(stopFlag) break;
+        }
+
+        return new int[]{firstNumIndex, secondNumIndex};
+    }
+}
